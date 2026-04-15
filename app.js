0954c99973b05c5f1dff7f160a6d73fd726af695
Supratimdas20 <supratimdaswork@gmail.com>
2026-04-15 23:22:35 +0530

    Planning the workflow

---
+++ app.js
@@ -0,0 +1,7 @@
+Step 1 → Create test case objects(Concepts: Objects, Data Types)
+Step 2 → Store them in an array(Concept: Arrays)
+Step 3 → Write a function to add a new test case (Concept: Variables)
+Step 4 → Filter failed tests using.filter()(Concept: Array Methods, Operators)
+Step 5 → Use if/switch to label priority (Concept: Control Flow)
+Step 6 → Loop through and print a summary report(Concept: Loops)
+Step 7 → Calculate pass % using arithmetic operators(Concept: Operators)
