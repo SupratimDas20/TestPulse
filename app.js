e462ae12cfd234c92dc131db1b0f80dd674b53ba
Supratimdas20 <supratimdaswork@gmail.com>
2026-04-24 23:41:28 +0530

    Adding Testcases

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const testCases3 = {
     error: "Not getting the desired result."
 }
 
+const testCases4 = {
+    id: 4,
+    name: "Search and Filter",
+    prority: "High",
+    status: "Fail",
+    error: "Not getting the desired result."
+
 Step 2 → Store them in an array(Concept: Arrays)
 Step 3 → Write a function to add a new test case (Concept: Variables)
 Step 4 → Filter failed tests using.filter()(Concept: Array Methods, Operators)
