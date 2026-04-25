453f2f3a3f6a8a8bd8354f11f98234faab7bdb1a
Supratimdas20 <supratimdaswork@gmail.com>
2026-04-25 23:17:39 +0530

    Adding TestCases.

--- app.js
+++ app.js
@@ -29,9 +29,17 @@ const testCases4 = {
     prority: "High",
     status: "Fail",
     error: "Not getting the desired result."
+}
+
+const testCases5 = {
+    id: 5,
+    name: "Navigation to different pages.",
+    prority: "High",
+    status: "Fail",
+    error: "Not getting the desired result."
 
 Step 2 → Store them in an array(Concept: Arrays)
-Step 3 → Write a function to add a new test case (Concept: Variables)
+Step 3 → Write a function to add a new test case(Concept: Variables)
 Step 4 → Filter failed tests using.filter()(Concept: Array Methods, Operators)
 Step 5 → Use if/switch to label priority (Concept: Control Flow)
 Step 6 → Loop through and print a summary report(Concept: Loops)
